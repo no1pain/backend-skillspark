cc86046f10de24f7a73ba9e1db8200db98da20f8
Oleksandr <fe.oleksandr.kazan@gmail.com>
2025-04-09 13:15:27 +0300

    fixes

--- server.js
+++ server.js
@@ -10,27 +10,29 @@ const PORT = process.env.PORT || 5174;
 
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://skillspark.vercel.app",
-      "https://skill-spark-bay.vercel.app",
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    allowedHeaders: [
-      "Content-Type",
-      "Authorization",
-      "Origin",
-      "Accept",
-      "X-Requested-With",
-      "Access-Control-Allow-Origin",
-    ],
-    credentials: true,
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
 app.use(express.json());
+
+app.use((req, res, next) => {
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Methods",
+      "GET, PUT, POST, DELETE, OPTIONS, PATCH"
+    );
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Authorization, X-Requested-With, Accept, Origin"
+    );
+    return res.status(200).json({});
+  }
+  next();
+});
 
 const courseRoutes = require("./routes/courseRoutes");
 const bookRoutes = require("./routes/bookRoutes");
