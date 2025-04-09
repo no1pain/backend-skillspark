5148f8d7d0d1322f7a77dd7ba33f0082b74eaa86
Oleksandr <fe.oleksandr.kazan@gmail.com>
2025-04-09 12:27:35 +0300

    Update server.js

--- server.js
+++ server.js
@@ -48,20 +48,6 @@ mongoose
     console.error("MongoDB connection error:", error);
   });
 
-app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  next();
-});
-
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
